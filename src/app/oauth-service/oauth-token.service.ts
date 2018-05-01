b0499fbe70c31485ec180d255e900e48836062ff
Samuel Shao <samuel.shao@qualifacts.com>
2018-04-30 20:35:47 -0500

    Added get access token service

--- src/app/oauth-service/oauth-token.service.ts
+++ src/app/oauth-service/oauth-token.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {Router} from "@angular/router";
 import {Http, Headers} from "@angular/http";
+import {RequestOptions} from "@angular/http";
 
 @Injectable()
 export class OauthTokenService {
@@ -9,16 +10,18 @@ export class OauthTokenService {
     private _router: Router, private _http: Http){}
 
   obtainAccessToken(clientId, clientSecret){
-    let params = {
-      'grant_type':'client_credentials'
-    };
-    // params.append('grant_type','password');
-    let headers = new Headers({'Content-type': 'application/x-www-form-urlencoded; charset=utf-8',
-      'Authorization': 'Basic '+ btoa(clientId + ":" + clientSecret)});
+    let urlSearchParams = new URLSearchParams();
+    urlSearchParams.set('grant_type', 'client_credentials');
 
-    console.log(btoa(clientId + ":" + clientSecret));
+    let body = urlSearchParams.toString();
+
+    let headers = new Headers({
+      'Content-type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic '+ btoa(clientId + ":" + clientSecret)
+    });
+
     let url = "http://localhost:9999/authorization/oauth/token";
-    this._http.post(url, params, {headers : headers})
+    this._http.post(url, body, {headers: headers})
       .toPromise()
       .then((data) => console.log(data))
       .catch();
